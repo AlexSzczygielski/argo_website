ca2d30e52d8d4e9b2cdf5cb779053f740a8992dc
AlexSzczygielski <theolekpl.szkolny@gmail.com>
2025-07-19 01:22:56 +0200

    navbar fix

--- index.php
+++ index.php
@@ -27,7 +27,53 @@
       <div class="col-sm-8">
         <h2>O nas</h2>
         <h4>Argo to klub regatowy współpracujący z AZS AGH...</h4>
-        <p>Czym jest argo?...</p>
+        <p>Czym jest argo?...
+          Litwo! Ojczyzno moja! ty jesteś jak zdrowie.
+          Ile cię trzeba cenić, ten tylko się dowie,
+          Kto cię stracił. Dziś piękność twą w całej ozdobie
+          Widzę i opisuję, bo tęsknię po tobie.
+
+          Panno Święta, co jasnej bronisz Częstochowy
+          I w Ostrej świecisz Bramie! Ty, co gród zamkowy
+          Nowogródzki ochraniasz z jego wiernym ludem!
+          Jak mnie dziecko do zdrowia powróciłaś cudem
+          (Gdy od płaczącej matki pod Twoją opiekę
+          Ofiarowany, martwą podniosłem powiekę
+          I zaraz mogłem pieszo do Twych świątyń progu
+          Iść za wrócone życie podziękować Bogu),
+          Tak nas powrócisz cudem na Ojczyzny łoże.
+
+          Tymczasem przenoś moją duszę utęsknioną
+          Do tych pagórków leśnych, do tych łąk zielonych,
+          Szeroko nad błękitnym Niemnem rozciągnionych;
+          Do tych pól malowanych zbożem rozmaitem,
+          Wyzłacanych pszenicą, posrebrzanych żytem;
+          Gdzie bursztynowy świerzop, gryka jak śnieg biała,
+          Gdzie pańieńskim rumieńcem żećmielina pała,
+          A wszystko przepasane, jakby wstęgą, miedzą
+          Zieloną, na niej z rzadka ciche grusze siedzą.
+
+          Śród takich pól przed laty, nad brzegiem ruczaju,
+          Na pagórku niewielkim, we brzozowym gaju,
+          Stał dwór szlachecki, z drzewa, lecz podmurowany;
+          Świeciły się z daleka pobielane ściany,
+          Tym bielsze, że odbite od ciemnej zieleni
+          Topoli, co go bronią od wiatrów jesieni.
+
+          Dom mieszkalny niewielki, lecz zewsząd chędożący,
+          I stodołę miał wielką, i przy niej trzy stogi
+          Użytku, co pod strzechą zmieścić się nie może;
+          Widać, że okolica obfita we zboże,
+          I widać z liczby kopic, co wzdłuż i wszerz smugów
+          Świecą gęsto jak gwiazdy, widać z liczby pługów
+          Orzących wcześnie łany ogromne ugoru,
+          Czarnoziemne, zapewne należne do dworu,
+          Uprawne dobrze na kształt ogrodowych grządek:
+          Że w tym domu dostatek mieszka i porządek.
+
+          Brama na wciąż otwarta przechodniom ogłasza,
+          Że gościnna i wszystkich w gościnę zaprasza.
+        </p>
         <button class="btn btn-secondary btn-lg">Get in Touch</button>
       </div>
       <div class="col-sm-4 text-center">
@@ -43,9 +89,53 @@
         <i class="fas fa-globe logo"></i> <!-- Font Awesome icon -->
       </div>
       <div class="col-sm-8">
-        <h2>Our Values</h2>
-        <h4><strong>MISSION:</strong> Our Amission lorem ipsum...</h4>
-        <p><strong>VISION:</strong> Our vision lorem ipsum...</p>
+        <h2>Wydarzenia</h2>
+        <h4><strong>1:</strong> Our Amission lorem ipsum...</h4>
+        <p><strong>2:</strong>Litwo! Ojczyzno moja! ty jesteś jak zdrowie.
+          Ile cię trzeba cenić, ten tylko się dowie,
+          Kto cię stracił. Dziś piękność twą w całej ozdobie
+          Widzę i opisuję, bo tęsknię po tobie.
+
+          Panno Święta, co jasnej bronisz Częstochowy
+          I w Ostrej świecisz Bramie! Ty, co gród zamkowy
+          Nowogródzki ochraniasz z jego wiernym ludem!
+          Jak mnie dziecko do zdrowia powróciłaś cudem
+          (Gdy od płaczącej matki pod Twoją opiekę
+          Ofiarowany, martwą podniosłem powiekę
+          I zaraz mogłem pieszo do Twych świątyń progu
+          Iść za wrócone życie podziękować Bogu),
+          Tak nas powrócisz cudem na Ojczyzny łoże.
+
+          Tymczasem przenoś moją duszę utęsknioną
+          Do tych pagórków leśnych, do tych łąk zielonych,
+          Szeroko nad błękitnym Niemnem rozciągnionych;
+          Do tych pól malowanych zbożem rozmaitem,
+          Wyzłacanych pszenicą, posrebrzanych żytem;
+          Gdzie bursztynowy świerzop, gryka jak śnieg biała,
+          Gdzie pańieńskim rumieńcem żećmielina pała,
+          A wszystko przepasane, jakby wstęgą, miedzą
+          Zieloną, na niej z rzadka ciche grusze siedzą.
+
+          Śród takich pól przed laty, nad brzegiem ruczaju,
+          Na pagórku niewielkim, we brzozowym gaju,
+          Stał dwór szlachecki, z drzewa, lecz podmurowany;
+          Świeciły się z daleka pobielane ściany,
+          Tym bielsze, że odbite od ciemnej zieleni
+          Topoli, co go bronią od wiatrów jesieni.
+
+          Dom mieszkalny niewielki, lecz zewsząd chędożący,
+          I stodołę miał wielką, i przy niej trzy stogi
+          Użytku, co pod strzechą zmieścić się nie może;
+          Widać, że okolica obfita we zboże,
+          I widać z liczby kopic, co wzdłuż i wszerz smugów
+          Świecą gęsto jak gwiazdy, widać z liczby pługów
+          Orzących wcześnie łany ogromne ugoru,
+          Czarnoziemne, zapewne należne do dworu,
+          Uprawne dobrze na kształt ogrodowych grządek:
+          Że w tym domu dostatek mieszka i porządek.
+
+          Brama na wciąż otwarta przechodniom ogłasza,
+          Że gościnna i wszystkich w gościnę zaprasza.</p>
       </div>
     </div>
   </div>
--- layout/navbar.php
+++ layout/navbar.php
@@ -27,3 +27,40 @@ if (pathinfo($request_uri, PATHINFO_EXTENSION) != 'php') {
     </div>
   </div>
 </nav>
+
+<script>
+$(document).ready(function(){
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+
+  // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 900, function(){
+
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+      });
+    } // End if
+  });
+
+  // Add scrolled class to navbar
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50) { // Adjust this value as needed
+            $('.navbar').addClass('scrolled');
+        } else {
+            $('.navbar').removeClass('scrolled');
+        }
+    });
+})
+</script>
