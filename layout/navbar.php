67e69b71301e2488ecd002d5a3fb5fd497e51d21
AlexSzczygielski <theolekpl.szkolny@gmail.com>
2025-07-19 01:13:01 +0200

    UI changes and logo files

--- layout/navbar.php
+++ layout/navbar.php
@@ -11,18 +11,18 @@ if (pathinfo($request_uri, PATHINFO_EXTENSION) != 'php') {
 
 <nav class="navbar navbar-expand-lg navbar-light fixed-top">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#home">ArgoLogo</a>
+    <a class="navbar-brand" href="#home">Argo</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNavbar" aria-controls="myNavbar" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="navbar-nav ms-auto">
-        <li class="nav-item"><a class="nav-link" href="#about">ABOUT</a></li>
-        <li class="nav-item"><a class="nav-link" href="#services">SERVICES</a></li>
-        <li class="nav-item"><a class="nav-link" href="#portfolio">PORTFOLIO</a></li>
-        <li class="nav-item"><a class="nav-link" href="#pricing">PRICING</a></li>
-        <li class="nav-item"><a class="nav-link" href="#contact">CONTACT</a></li>
+        <li class="nav-item"><a class="nav-link" href="#about">O nas</a></li>
+        <li class="nav-item"><a class="nav-link" href="#about">Wydarzenia</a></li>
+        <li class="nav-item"><a class="nav-link" href="#services">Współpraca</a></li>
+        <li class="nav-item"><a class="nav-link" href="#portfolio">Dołącz</a></li>
+        <li class="nav-item"><a class="nav-link" href="#contact">Kontakt</a></li>
       </ul>
     </div>
   </div>
