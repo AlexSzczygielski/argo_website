48df50c195341d79986c3a16b751a246a0037797
AlexSzczygielski <theolekpl.szkolny@gmail.com>
2025-07-29 22:01:34 +0200

    New header

---
+++ layout/header.php
@@ -0,0 +1,25 @@
+<?php
+	$app_version = '2025.02.04.2';
+?>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="icon" href="storage/images/favicon.ico" />
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat%3A600%2C100%2C100italic%2C200%2C200italic%2C300%2C300italic%2Cregular%2Citalic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CMuli%3A200%2C200italic%2C300%2C300italic%2Cregular%2Citalic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic&subset=cyrillic%2Clatin-ext,latin-ext&display=swap" media="all" onload="this.media='all'">
+<link rel="stylesheet" type="text/css" href="css/style.css?ver=<?php echo $app_version; ?>">
+<link rel="stylesheet" type="text/css" href="plugins/bootstrap/5.3.3/css/bootstrap.css?ver=<?php echo $app_version; ?>">
+<link rel="stylesheet" type="text/css" href="fontawesome6/css/all.css?ver=<?php echo $app_version; ?>">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Cinzel:wght@600&family=Inter:wght@300;400&display=swap" media="all" onload="this.media='all'">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lora:wght@600&display=swap" media="all" onload="this.media='all'">
+<script src="plugins/jquery/jquery-3.7.1.min.js?ver=<?php echo $app_version; ?>"></script>
+<script src="plugins/bootstrap/5.3.3/js/bootstrap.js?ver=<?php echo $app_version; ?>"></script>
+
+
+<!-- Global site tag (gtag.js) - Google Analytics
+<script async src="https://www.googletagmanager.com/"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '');
+</script> -->
