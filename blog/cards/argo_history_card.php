0cfd4634c17124e79b6bc7c459554eaf38e55356
AlexSzczygielski <theolekpl.szkolny@gmail.com>
2025-08-01 01:25:10 +0200

    fixed cards text messages

--- blog/cards/argo_history_card.php
+++ blog/cards/argo_history_card.php
@@ -1,8 +1,8 @@
 <div class="card mb-4 shadow-sm">
     <img src="storage/images/argo_painting.jpg" class="card-img-top" alt="Wydarzenie">
     <div class="card-body">
-        <h5 class="card-title">Historia Argo</h5>
-        <p class="card-text">Poznaj historię naszego klubu, od jego początków po dzień dzisiejszy. Zobacz, jak ewoluowało nasze zaangażowanie w żeglarstwo.</p>
+        <h5 class="card-title">Czym właściwie jest Argo?</h5>
+        <p class="card-text">Czyli dlaczego "nie należy zaniedbywać nauki o pięknie"?</p>
         <a href="#" class="btn btn-sm btn-outline-secondary">Czytaj więcej</a>
     </div>
 </div>
--- blog/cards/ostatnie_starty.php
+++ blog/cards/ostatnie_starty.php
@@ -1,8 +1,8 @@
 <div class="card mb-4 shadow-sm">
     <img src="storage/images/sea2.jpg" class="card-img-top" alt="Wydarzenie">
     <div class="card-body">
-        <h5 class="card-title">Ostatnie starty</h5>
-        <p class="card-text">Podsumowanie ostatnich startów i osiągnięć naszych załóg. Zobacz, gdzie rywalizowaliśmy i jakie sukcesy odnieśliśmy.</p>
+        <h5 class="card-title">Nasze ostatnie starty</h5>
+        <p class="card-text">Podsumowanie ostatnich startów i osiągnięć naszych załóg. Zobacz, gdzie rywalizowaliśmy i jakie odnieśliśmy sukcesy.</p>
         <a href="#" class="btn btn-sm btn-outline-secondary">Czytaj więcej</a>
     </div>
 </div>
