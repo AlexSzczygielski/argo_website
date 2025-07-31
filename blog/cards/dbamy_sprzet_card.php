43f99333cdc4bb27607286aa28bd396fbbd6f2ec
AlexSzczygielski <theolekpl.szkolny@gmail.com>
2025-08-01 00:47:24 +0200

    Card text center fixed

--- blog/cards/dbamy_sprzet_card.php
+++ blog/cards/dbamy_sprzet_card.php
@@ -1,7 +1,7 @@
 <div class="col-6 col-md-4">
     <div class="card mb-4 shadow-sm">
         <img src="storage/images/2024/boat_sleep.webp" class="card-img-top" alt="Regaty">
-        <div class="card-body text-center">
+        <div class="card-body">
             <h5 class="card-title">Jak dbamy o sprzęt?</h5>
             <p class="card-text">Uchylamy rąbka tajemnicy przygotowania najszybszych akademickich łódek.</p>
             <a href="#" class="btn btn-sm btn-outline-secondary">Czytaj więcej</a>
