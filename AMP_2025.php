9d4e2e93c96f7881835693a5be6771d1fab8e7d3
AlexSzczygielski <theolekpl.szkolny@gmail.com>
2025-08-02 01:48:26 +0200

    href upwind fix

--- AMP_2025.php
+++ AMP_2025.php
@@ -26,7 +26,7 @@
                             <ul class="list-unstyled">
                                 <li><strong>36.</strong> Wiktor Leśkiewicz, Kacper Ćwiokowski, Hubert Kraj</li>
                                 <li><strong>62.</strong> Maciej Szewczuk, Antoni Jaczyński, Jan Jóskow</li>
-                                <li><strong><a href="https.www.upwind24.pl/amp-2025/results" target="_blank">Pełne wyniki na stronie Upwind</a></strong></li>
+                                <li><strong><a href="https://www.upwind24.pl/amp-2025/results" target="_blank">Pełne wyniki na stronie Upwind</a></strong></li>
                             </ul>
                         </div>
 
