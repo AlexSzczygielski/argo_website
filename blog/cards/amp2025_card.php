ddd2d57396f36bf344cffc6ef1767372e707b29b
AlexSzczygielski <theolekpl.szkolny@gmail.com>
2025-08-02 01:43:35 +0200

    quick fix

--- blog/cards/amp2025_card.php
+++ blog/cards/amp2025_card.php
@@ -3,6 +3,6 @@
     <div class="card-body">
         <h5 class="card-title">Akademickie Mistrzostwa Polski w Żeglarstwie 2025</h5>
         <p class="card-text">Zobacz relację ze startu Argonautów w Akademickich Mistrzostwach Polski w Żeglarstwie.</p>
-        <a href="blog/AMP_2025.php" class="btn btn-sm btn-outline-secondary">Czytaj więcej</a>
+        <a href="AMP_2025.php" class="btn btn-sm btn-outline-secondary">Czytaj więcej</a>
     </div>
 </div>
