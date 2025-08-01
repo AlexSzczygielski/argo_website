170e1b42dab4fa6bc0e7b5f08f9b648ce03ccf35
AlexSzczygielski <theolekpl.szkolny@gmail.com>
2025-08-02 01:34:50 +0200

    photo in AMP_2025.php

--- AMP_2025.php
+++ AMP_2025.php
@@ -17,19 +17,26 @@
 
             <!-- Page content starts here -->
             <div class="container p-4">
-                <h1 class="mt-5">Akademickie Mistrzostwa Polski w Żeglarstwie 2025</h1>
-                <p class="lead">W tym roku Argo do najważniejszych regat w sezonie wystawiło dwie załogi.</p>
-                <div class="mt-5">
-                    <h4 class="mt-5">Wyniki:</h4>
-                    <ul class="list-unstyled">
-                        <li><strong>36.</strong> Wiktor Leśkiewicz, Kacper Ćwiokowski, Hubert Kraj</li>
-                        <li><strong>62.</strong> Maciej Szewczuk, Antoni Jaczyński, Jan Jóskow</li>
-                        <li><strong><a href="https://www.upwind24.pl/amp-2025/results" target="_blank">Pełne wyniki na stronie Upwind</a></strong></li>
-                    </ul>
-                </div>
+                <div class="row">
+                    <div class="col-md-7">
+                        <h1 class="mt-5">Akademickie Mistrzostwa Polski w Żeglarstwie 2025</h1>
+                        <p class="lead">W tym roku Argo do najważniejszych regat w sezonie wystawiło dwie załogi.</p>
+                        <div class="mt-5">
+                            <h4 class="mt-5">Wyniki:</h4>
+                            <ul class="list-unstyled">
+                                <li><strong>36.</strong> Wiktor Leśkiewicz, Kacper Ćwiokowski, Hubert Kraj</li>
+                                <li><strong>62.</strong> Maciej Szewczuk, Antoni Jaczyński, Jan Jóskow</li>
+                                <li><strong><a href="https.www.upwind24.pl/amp-2025/results" target="_blank">Pełne wyniki na stronie Upwind</a></strong></li>
+                            </ul>
+                        </div>
 
-                <div class="mt-5 text-muted" style="font-style: italic; font-size: 0.9rem;">
-                    AUTOR: GALL ANONIM
+                        <div class="mt-5 text-muted" style="font-style: italic; font-size: 0.9rem;">
+                            AUTOR: GALL ANONIM
+                        </div>
+                    </div>
+                    <div class="col-md-5 text-right mt-4 mt-md-0">
+                        <img src="storage/images/2025/AMP25.jpg" alt="AMP 2025 Regaty" class="img-fluid">
+                    </div>
                 </div>
             </div>
             <!-- Page content ends here -->
