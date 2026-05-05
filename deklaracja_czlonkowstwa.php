a79ce061fbd169da0c17e74e6c415b050d13a098
AlexSzczygielski <a.szczygielski21@gmail.com>
2026-05-05 17:25:32 +0200

    deleted old php file, not used anymore, added deklaracja czlonkowstwa subpage

---
+++ deklaracja_czlonkowstwa.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <title>SKR Argo AGH - Deklaracja członkowstwa</title>
+    <?php require_once('layout/header.php'); ?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="description" content="SKR ARGO AGH">
+</head>
+
+<body>
+
+    <div class="page-container">
+        <div class="content-wrap">
+            <?php require_once('layout/navbar.php'); ?>
+
+            <!-- Page content starts here -->
+            <div class="container p-4">
+                <h1 class="mt-5 mb-4">Deklaracja członkowstwa</h1>
+
+                <div class="row">
+                    <!-- Left column: text -->
+                    <div class="col-md-8">
+                        <section class="mb-5">
+                            <p class="lead">
+                                Warunkiem członkostwa w Studenckim Kole Regatowym Argo AGH jest wypełnienie deklaracji członkowskiej. Wypełnienie deklaracji członkowskiej jest możliwe wyłącznie dla osób, które pozytywnie przeszły procedurę akceptacji do klubu.
+
+                            </p>
+                        </section>
+
+                        <section class="mb-5">
+                            <h2>Deklaracje</h2>
+                            <h4>2025/2026</h4>
+                            <ul>
+                                <li>MS Forms: <a href="https://forms.cloud.microsoft/Pages/ResponsePage.aspx?id=PwOxgOAhgkq7wPBf3M07yM-Xa0THU7pMhLrElNwbQopUMEYyTkJWOUhZUE02ME9RMk1XUUZWSzNXSi4u">https://forms.cloud.microsoft/Pages/ResponsePage.aspx?id=PwOxgOAhgkq7wPBf3M07yM-Xa0THU7pMhLrElNwbQopUMEYyTkJWOUhZUE02ME9RMk1XUUZWSzNXSi4u</a></li>
+                            </ul>
+                        </section>
+
+                        <div class="mt-5 text-muted" style="font-style: italic; font-size: 0.9rem;">
+                            UWAGA: Deklaracje należy wypełniać z maila w domenie student.agh.edu.pl !
+                        </div>
+                    </div>
+
+                    <!-- Right column: images -->
+                    <div class="col-md-4">
+                        <img src="storage/images/sail.png" alt="Inne zdjęcie" class="img-fluid rounded mb-4">
+                    </div>
+                </div>
+            </div>
+            <!-- Page content ends here -->
+
+        </div>
+        <?php require_once('layout/footer.php'); // Include the footer 
+        ?>
+    </div>
+
+</body>
+
+</html>
