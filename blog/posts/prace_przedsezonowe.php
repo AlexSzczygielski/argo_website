dd704d3387eda146ed5ed464db64a14b43f4eb72
AlexSzczygielski <a.szczygielski21@gmail.com>
2026-06-07 12:41:51 +0200

    #5 migration to sql - clearing old files

--- blog/posts/prace_przedsezonowe.php
+++
@@ -1,6 +0,0 @@
-<p>Przed sezonem dbamy dokładnie o nasz sprzęt! Łódki czyste i wypolerowane pływają szybciej, więc członkowie ARGO zaangażowali się do pracy.</p>
-
-<div class="mt-5 text-muted"
-    style="font-style: italic; font-size: 0.9rem;">
-    AUTOR: <?= htmlspecialchars($post['author'] ?? 'ARGO') ?>
-</div>
